784858bf3fc0a22e1da978f42ed23434e08aa4fc
James ADJINWA <jaymee126@gmail.com>
2023-10-09 22:53:23 -0600

    #28 enabled static website hosting, uploaded index and error files (#29)

--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -16,3 +16,23 @@ variable "bucket_name" {
     error_message = "Invalid S3 bucket name. It must consist of only letters, numbers, hyphens, or underscores."
   }
 }
+
+variable "index_html_filepath" {
+  description = "The file path for index.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.index_html_filepath)
+    error_message = "The provided path for index.html does not exist."
+  }
+}
+
+variable "error_html_filepath" {
+  description = "The file path for error.html"
+  type        = string
+
+  validation {
+    condition     = fileexists(var.error_html_filepath)
+    error_message = "The provided path for error.html does not exist."
+  }
+}
